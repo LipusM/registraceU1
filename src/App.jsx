b3d244b85aad761dc0c26b32407f87e938a28940
LipusM <mlipus007@gmail.com>
2023-10-02 18:08:28 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+const c = console.log.bind(document)
 
 import './App.scss'
+
+import Registration from './components/Registration'
 
 const App = () => {
 
 
   return (
     <>
-
+      <Registration/>
     </>
   )
 }
---
+++ src/components/Registration/index.jsx
@@ -0,0 +1,14 @@
+const c = console.log.bind(document)
+
+import "./style.scss"
+import { useState, useEffect } from "react"
+
+const Registration = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Registration
