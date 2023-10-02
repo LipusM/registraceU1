c5514d386be13ead2e59d7ff6d745d8ad804a1af
LipusM <mlipus007@gmail.com>
2023-10-02 18:53:57 +0200

    Stylování formuláře

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -1,13 +1,27 @@
 const c = console.log.bind(document)
 
 import "./style.scss"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 const Registration = () => {
+
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+    })
+
     return (
-        <>
-        
-        </>
+        <section>
+            <form id="registrationForm">
+                <input type="text" placeholder="Email address"/>
+                <input type="text" placeholder="User name"/>
+                <input type="text" placeholder="Password"/>
+                <input type="text" placeholder="Confirm password"/>
+                <button type="submit">Register</button>
+            </form>
+        </section>
     )
 }
 
